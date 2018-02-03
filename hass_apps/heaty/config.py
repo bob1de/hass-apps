bb96f859b0b8310207c5ccd6612bf278d5731022
Robert Schindler <r.schindler@efficiosoft.com>
2018-02-03 12:48:52 +0100

    [heaty] Simplified parsing of schedule rule constraints from config

--- hass_apps/heaty/config.py
+++ hass_apps/heaty/config.py
@@ -7,9 +7,8 @@ import voluptuous as vol
 from . import schedule, util
 
 
-# all constraints that have values in the range_string format
-# (see util.expand_range_string)
-RANGE_STRING_CONSTRAINTS = ("years", "months", "days", "weeks", "weekdays")
+# names of schedule rule constraints to be fetched from the rule definition
+CONSTRAINTS = ("years", "months", "days", "weeks", "weekdays")
 
 
 def build_schedule_rule(rule):
@@ -18,8 +17,8 @@ def build_schedule_rule(rule):
 
     constraints = {}
     for name, value in rule.items():
-        if name in RANGE_STRING_CONSTRAINTS:
-            constraints[name] = util.expand_range_string(value)
+        if name in CONSTRAINTS:
+            constraints[name] = value
 
     start_time = rule.get("start")
     if start_time is not None:
@@ -70,7 +69,13 @@ def config_post_hook(cfg):
 
 ENTITY_ID_SCHEMA = vol.Schema(vol.Match(r"^[A-Za-z_]+\.[A-Za-z0-9_]+$"))
 PYTHON_VAR_SCHEMA = vol.Schema(vol.Match(r"^[a-zA-Z_]+[a-zA-Z0-9_]*$"))
-RANGE_STRING_SCHEMA = vol.Schema(vol.Match(r"^ *\d+( *\- *\d+)?( *\, *\d+( *\- *\d+)?)* *$"))
+RANGE_STRING_SCHEMA = vol.Schema(vol.All(
+    vol.Any(
+        int,
+        vol.Match(r"^ *\d+( *\- *\d+)?( *\, *\d+( *\- *\d+)?)* *$"),
+    ),
+    util.expand_range_string,
+))
 TIME_SCHEMA = vol.Schema(vol.Match(r"^ *([01]\d|2[0123]) *\: *([012345]\d) *$"))
 
 TEMP_SCHEMA = vol.Schema(vol.Any(float, int, "off"))
@@ -84,7 +89,7 @@ TEMP_EXPRESSION_MODULES_SCHEMA = vol.Schema({
 })
 
 THERMOSTAT_SCHEMA = vol.Schema({
-    vol.Optional("delta", default=0): float,
+    vol.Optional("delta", default=0): vol.Any(float, int),
     vol.Optional("min_temp", default=None): vol.Any(TEMP_SCHEMA, None),
     vol.Optional("set_temp_retries", default=10):
         vol.All(int, vol.Range(min=-1)),
@@ -118,11 +123,11 @@ SCHEDULE_RULE_SCHEMA = vol.Schema(vol.All({
     vol.Optional("start", default=None): vol.Any(TIME_SCHEMA, None),
     vol.Optional("end", default=None): vol.Any(TIME_SCHEMA, None),
     vol.Optional("end_plus_days", default=0): vol.All(int, vol.Range(min=0)),
-    vol.Optional("years"): vol.Any(RANGE_STRING_SCHEMA, int),
-    vol.Optional("months"): vol.Any(RANGE_STRING_SCHEMA, int),
-    vol.Optional("days"): vol.Any(RANGE_STRING_SCHEMA, int),
-    vol.Optional("weeks"): vol.Any(RANGE_STRING_SCHEMA, int),
-    vol.Optional("weekdays"): vol.Any(RANGE_STRING_SCHEMA, int),
+    vol.Optional("years"): RANGE_STRING_SCHEMA,
+    vol.Optional("months"): RANGE_STRING_SCHEMA,
+    vol.Optional("days"): RANGE_STRING_SCHEMA,
+    vol.Optional("weeks"): RANGE_STRING_SCHEMA,
+    vol.Optional("weekdays"): RANGE_STRING_SCHEMA,
 }, build_schedule_rule))
 SCHEDULE_SCHEMA = vol.Schema(vol.All([SCHEDULE_RULE_SCHEMA], build_schedule))
 
