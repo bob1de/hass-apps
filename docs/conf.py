5fad5ad60d73d401a04a89984a23bc88dfce3e60
Robert Schindler <r.schindler@efficiosoft.com>
2018-12-23 17:16:47 +0100

    Limit sidebar navigation depth to 3 and mark external links in docs

--- docs/conf.py
+++ docs/conf.py
@@ -88,6 +88,10 @@ html_theme = 'sphinx_rtd_theme'
 # documentation.
 #
 # html_theme_options = {}
+html_theme_options = {
+    'navigation_depth': 3,
+    'style_external_links': True,
+}
 
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
